9a022fe54f70a6a8bc7dc19eab47be39fa52d153
despicablemme <yuhang-guo@foxmail.com>
2025-02-21 21:21:47 +0800

    init project

---
+++ wuziqi/app/src/main/java/com/gyh/wuziqi/ui/adapter/CheeseBoardAdapter.java
@@ -0,0 +1,25 @@
+package com.gyh.wuziqi.ui.adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CheeseBoardAdapter extends RecyclerView.Adapter {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
